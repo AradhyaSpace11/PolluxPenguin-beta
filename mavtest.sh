4453d8771b436e40bf499e6fcf0806b6f736dd9d
Aradhya Gaonkar <iloveastronomy11@gmail.com>
2024-09-01 23:44:15 +0530

    some more stuff

--- mavtest.sh
+++ mavtest.sh
@@ -7,9 +7,9 @@ gnome-terminal --tab -- bash -c "roslaunch gazebo_ros multi.launch; exec $SHELL"
 sleep 1
 gnome-terminal --tab -- bash -c "cd ~/ardupilot/Tools/autotest && ./sim_vehicle.py -v ArduCopter -f gazebo-iris -I0; exec $SHELL" &
 sleep 1
-gnome-terminal --tab -- bash -c "python3 control2.py; exec $SHELL" &
+gnome-terminal --tab -- bash -c "python3 controller.py; exec $SHELL" &
 sleep 2
-gnome-terminal --tab -- bash -c "python3.10 gpt7.py; exec $SHELL" &
+gnome-terminal --tab -- bash -c "python3.10 llm_communicator.py; exec $SHELL" &
 sleep 1
 gnome-terminal --tab -- bash -c "python3 detector.py; exec $SHELL" &
 sleep 1
