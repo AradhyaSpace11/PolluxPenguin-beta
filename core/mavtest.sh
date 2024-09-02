6dde25894e9bb259f000e2abd6b70e562432f08c
Aradhya Ajit Gaonkar <63801518+AradhyaSpace11@users.noreply.github.com>
2024-09-02 14:44:57 +0530

    Add files via upload
    added sh

---
+++ core/mavtest.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Change to the PolluxPenguin directory
+cd "$HOME/PolluxPenguin-beta/core" || exit
+
+gnome-terminal --tab -- bash -c "roslaunch gazebo_ros multi.launch; exec $SHELL" &
+sleep 1
+gnome-terminal --tab -- bash -c "cd ~/ardupilot/Tools/autotest && ./sim_vehicle.py -v ArduCopter -f gazebo-iris -I0; exec $SHELL" &
+sleep 1
+gnome-terminal --tab -- bash -c "python3 controller.py; exec $SHELL" &
+sleep 2
+gnome-terminal --tab -- bash -c "python3.10 llm_communicator.py; exec $SHELL" &
+sleep 1
+gnome-terminal --tab -- bash -c "python3 detector.py; exec $SHELL" &
+sleep 1
+gnome-terminal --tab -- bash -c "python3 chfifo.py; exec $SHELL" &
+sleep 1
+gnome-terminal --tab -- bash -c "python3.10 vision.py; exec $SHELL" &
