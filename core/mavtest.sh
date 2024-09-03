c5de6de6a00607b6eb1266c38abebae9906f0466
AradhyaSpace11 <iloveastronomy11@gmail.com>
2024-09-03 19:49:59 +0530

    added a better .desktop

--- core/mavtest.sh
+++ core/mavtest.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+
+source /opt/ros/noetic/setup.bash
+
 # Change to the PolluxPenguin directory
 cd "$HOME/PolluxPenguin-beta/core" || exit
 
-gnome-terminal --tab -- bash -c "roslaunch gazebo_ros multi.launch; exec $SHELL" &
-sleep 1
 gnome-terminal --tab -- bash -c "cd ~/ardupilot/Tools/autotest && ./sim_vehicle.py -v ArduCopter -f gazebo-iris -I0; exec $SHELL" &
 sleep 1
+gnome-terminal --tab -- bash -c "roslaunch gazebo_ros multi.launch; exec $SHELL" &
+sleep 8
 gnome-terminal --tab -- bash -c "python3 controller.py; exec $SHELL" &
 sleep 2
 gnome-terminal --tab -- bash -c "python3.10 llm_communicator.py; exec $SHELL" &
